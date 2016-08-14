a9643b0b233160aaef2a074bac5fdbd218cf7527
Wolfgang Fahl <wf@bitplan.com>
2016-08-14 12:19:16 +0200

    improves pom to upgrade to latest JNA

--- src/edsdk/api/CanonCamera34.java
+++ src/edsdk/api/CanonCamera34.java
@@ -1,7 +1,8 @@
 package edsdk.api;
 
 import com.sun.jna.Native;
-
+import com.sun.jna.Library;
+import com.sun.jna.Function;
 import edsdk.bindings34.EdSdkLibrary;
 /**
  * this class is a EDSDK Version 3.4 implementation that should work on MacOSX
@@ -14,6 +15,8 @@ public class CanonCamera34 extends BaseCanonCamera {
 
     static {
         initLibrary();
+        // options.put(Library.OPTION_CALLING_CONVENTION, Function.ALT_CONVENTION);
+        // options.put(Library.OPTION_CALLING_CONVENTION, Function.C_CONVENTION);
         EDSDK = (EdSdkLibrary) Native.loadLibrary( edsdkDllLoc, EdSdkLibrary.class, options );
     }
 }
