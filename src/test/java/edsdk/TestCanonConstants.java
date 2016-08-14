acaa195bc0c79c3ba88e4bdb49bcfdccde6b5e6f
Wolfgang Fahl <wf@bitplan.com>
2016-08-14 19:00:36 +0200

    adds JUnit test

---
+++ src/test/java/edsdk/TestCanonConstants.java
@@ -0,0 +1,14 @@
+package edsdk;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCanonConstants {
+
+  @Test
+  public void testConstants() {
+    fail("Not yet implemented");
+  }
+
+}
